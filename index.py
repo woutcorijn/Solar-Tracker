f06e0b2654e3a2fff2a48ed158fa2e8cfdc120e7
woutcorijn <26119233+woutcorijn@users.noreply.github.com>
2020-12-22 21:01:46 +0100

    V0.02

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
+#module for reading serial data.
 import serial
+#webserver module.
+from flask import Flask
 
+#Open the Serial Port
 ser = serial.Serial('/dev/rfcomm0', 9600)
 
 print("Waiting for data...")
@@ -7,3 +11,10 @@ print("Waiting for data...")
 while True:
     result = ser.readline()
     print(result)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
