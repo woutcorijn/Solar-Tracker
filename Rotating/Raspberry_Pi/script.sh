2065789ddb52aa23b8d53037865efb729a947b4c
Woutcorijn <26119233+woutcorijn@users.noreply.github.com>
2021-04-12 15:38:39 +0100

    V0.35

--- Rotating/Raspberry_Pi/script.sh
+++ Rotating/Raspberry_Pi/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/woutco/Solar-Tracker/Raspberry_Pi/
+cd /home/woutco/Solar-Tracker/Rotating/Raspberry_Pi/
 rfcomm release 0
 rfcomm bind hci0 20:16:06:15:63:62
 timeout 2h python index.py
